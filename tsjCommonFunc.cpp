ab6c6a3ae70be0847d5c9acb844790c5cdd5c10c
supertan <943648187@qq.com>
2022-02-19 00:32:45 +0800

    bugs remain

---
+++ tsjCommonFunc.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Shaojie Tan
+ * @Date: 2022-02-18 23:45:02
+ * @LastEditors: Shaojie Tan
+ * @LastEditTime: 2022-02-19 00:11:59
+ */
+
+
+#include "tsjCommonFunc.h"
+
+
+
+void errorPrint(char const * str){
+    std::cout << FRED(str) << endl;
+}
+
+
+void func()
+{
+// do something
+
+}
